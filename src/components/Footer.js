bed56104cfca1fd909d68c0d5ebd95390f5acef6
Quenton Ni <ni000100@umn.edu>
2025-10-27 15:36:25 -0500

    Push other work

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer({ setPage }) {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section footer-info">
+          <p className="footer-copyright">Quenton Ni</p>
+          <p className="footer-updated">Last updated: October 2025</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
